a5a330034ea26fc9a706b2b97be5d11798a82edd
Ilya Lonshakov <modnyi-l@mail.ru>
2026-08-21 17:55:01 +0700

    feat(backend): add celebrate validation for product and order

---
+++ backend/src/middlewares/validation/order.ts
@@ -0,0 +1,12 @@
+import { Joi, Segments } from 'celebrate';
+
+export const createOrderValidator = {
+  [Segments.BODY]: Joi.object().keys({
+    payment: Joi.string().valid('card', 'online').required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+    address: Joi.string().required(),
+    total: Joi.number().required(),
+    items: Joi.array().items(Joi.string().hex().length(24)).min(1).required(),
+  }),
+};
