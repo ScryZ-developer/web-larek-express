6a5a35cfe15129862379ef9f9c3fb4cb286c0735
Ilya Lonshakov <modnyi-l@mail.ru>
2026-08-21 17:55:01 +0700

    feat(backend): add custom http error classes

---
+++ backend/src/errors/default-error.ts
@@ -0,0 +1,10 @@
+class DefaultError extends Error {
+  statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.statusCode = 500;
+  }
+}
+
+export default DefaultError;
